4195f20a6b0b2de94213164ff0f28da73e76bd85
Khaxtran <khatran961217@gmail.com>
2021-07-28 13:56:40 +1000

    Added search feature
    Search engine now can search through posts in selected subcreddit

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import { FaRedditSquare, FaSearch } from 'react-icons/fa';
 import './Header.css';
 
+import {setSearchTerm} from '../../app/credditSlice';
+
 
 export const Header = () => {
+    const [searchTermLocal, setSearchTermLocal] = useState('');
+    const searchTerm = useSelector(state => state.creddit.searchTerm);
+    const dispatch = useDispatch();
+
+    const handleTermChange = (e) => {
+        setSearchTermLocal(e.target.value);
+    };
+
+    useEffect(() => {
+        setSearchTermLocal(searchTerm);
+    }, [searchTerm]);
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+        dispatch(setSearchTerm(searchTermLocal));
+    };
+
     return (
         <header>
             <div className="logo-container">
@@ -12,9 +32,19 @@ export const Header = () => {
                 <p className='reddit-logo'>reddit</p>
             </div>
             <div className='search-section' >
-                <form>
-                    <input className='search-bar' type='text' placeholder='Search'/>
-                    <button type='submit' aria-label='Search'>
+                <form onSubmit={onSearchSubmit}>
+                    <input 
+                        className='search-bar' 
+                        type='text' 
+                        placeholder='Search'
+                        value={searchTermLocal}
+                        onChange={handleTermChange}
+                        aria-label='Search posts'/>
+                    <button 
+                        type='submit'
+                        onClick={onSearchSubmit} 
+                        aria-label='Search'
+                        >
                         <FaSearch />
                     </button>
                 </form>
